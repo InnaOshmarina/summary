151af59984f92fb5fa805a7ffb08cade2a440f70
Inna <inna.rose2305@gmail.com>
2018-10-09 14:14:02 +0300

    example commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-const inna = 28;
+/* eslint-disable react/prefer-stateless-function */
+import React, { PureComponent } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+const Italy = 28;
 
-class App extends Component {
+class App extends PureComponent {
   render() {
-
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            Welcome to React
+            {Italy}
+          </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
